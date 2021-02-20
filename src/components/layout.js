846d031fe1308a3738aac86de030ab0d7be2339b
sof <abril.pinneiro@gmail.com>
2021-02-20 02:12:23 -0300

    manifest 1h

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,10 +10,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-
+import Navbar from "./Globals/Navbar";
 import "./layout.css"
 
-const Layout = ({ children }) =>  <> {children} </>;
+const Layout = ({ children }) => (
+<> 
+<Navbar />
+{children} </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
