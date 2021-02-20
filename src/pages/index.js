f5ce784816ddaf9d25d312837e0d4adc6ec35bf8
sof <abril.pinneiro@gmail.com>
2021-02-19 21:53:40 -0300

    git head

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
+import {
+  FaGulp 
+}from "react-icons/fa";
+import iconserafin from"../images/iconserafin.jpg";
 
 const IndexPage = () => (
   <Layout>
@@ -11,12 +15,10 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  <FaGulp/>
+  <img style={{ width:`39px` }} src={iconserafin }/>
   </Layout>
-)
+
+);
 
 export default IndexPage
